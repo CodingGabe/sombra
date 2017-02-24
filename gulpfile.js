7ab6c51de2c0376d3e1c71f5a9dbeb36ea2cc075
Gabe Amaya <GabeAmaya2@gmail.com>
2017-02-23 22:43:23 -0600

    Initial commit for new project

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('workflow', function () {
+	gulp.src('./src/sass/**/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(cssnano())
+		.pipe(sourcemaps.write('./'))
+
+	.pipe(gulp.dest('./dist/css/'))
+});
+
+gulp.task('default', function () {
+	gulp.watch('./src/sass/**/*.scss', ['workflow']);
+});
